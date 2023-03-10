842dd311c25cfdf65665e3a5c62e75c0906b735a
2kai2kai2 <2kai2kai2@gmail.com>
2023-03-09 20:37:10 -0500

    AES documentation for python

--- lib/wrapper/__init__.pyi
+++ lib/wrapper/__init__.pyi
@@ -1,7 +1,30 @@
 
 def encrypt_block(msg: bytes, key: bytes) -> bytes:
     """
-    aaa
+    Encrypts a single block using AES.
+
+    Parameters
+    ----
+      - `msg` - A 128-bit (16-byte) plaintext block to encrypt
+
+      - `key` - A 256-bit (32-byte) symmetric key
+    
+    Returns
+    ----
+    The cooresponding 128-bit (16-byte) ciphertext block
     """
     
-def decrypt_block(msg: bytes, key: bytes) -> bytes: ...
+def decrypt_block(msg: bytes, key: bytes) -> bytes:
+    """
+    Decrypts a single block using AES.
+
+    Parameters
+    ----
+      - `msg` - A 128-bit (16-byte) ciphertext block to encrypt
+
+      - `key` - A 256-bit (32-byte) symmetric key
+    
+    Returns
+    ----
+    The cooresponding 128-bit (16-byte) plaintext block
+    """
