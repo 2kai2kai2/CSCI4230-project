25bb7a53c250f0902f50c1d31ac1ebfee926a7e8
2kai2kai2 <2kai2kai2@gmail.com>
2023-03-16 17:51:45 -0400

    Basic database and framework
    Server now ensures an order for when messages can be handled:
    1. SSL handshake to establish session_key
    2. Account/card authentication
    3. Client commands permitted

---
+++ database.py
@@ -0,0 +1,105 @@
+"""
+This file provides a simple abstraction of a database attached to the server.
+"""
+from shared.card import Card
+
+
+class Account:
+    """
+    Represents a bank account.
+
+    `balance` is in cents.
+    """
+
+    def __init__(self, name: str, card: Card, balance: int = 0):
+        self.name = name
+        self.card = card
+        self._balance = balance
+
+    @property
+    def balance(self) -> int:
+        """
+        Account balance in cents.
+        """
+        return self._balance
+
+    def deposit(self, amount: int):
+        """
+        Adds money to the account.
+
+        Always succeeds.
+
+        Parameters
+        ----
+           - `amount` in cents to be added to the balance.
+
+        Throws
+        ----
+           -`ValueError` if a negative amount is specified.
+        """
+        if amount < 0:
+            raise ValueError("Cannot deposit a negative amount.")
+        self._balance += amount
+
+    def withdraw(self, amount: int):
+        """
+        Removes money from the account.
+
+        Parameters
+        ----
+           - `amount` in cents to be removed from the balance.
+
+        Throws
+        ----
+           - `RuntimeError` if there are insufficient funds.
+           - `ValueError` if a negative amount is specified.
+        """
+        if amount < 0:
+            raise ValueError("Cannot withdraw a negative amount.")
+        elif amount > self._balance:
+            raise RuntimeError("Insufficient Funds.")
+        self._balance -= amount
+
+
+_db: dict[str, Account] = {}
+
+
+def _add_account(name: str, card: Card, balance: int):
+    if card.number in _db:
+        raise RuntimeError("Card number already exists.")
+    _db[card.number] = Account(name, card, balance)
+
+
+_add_account("Mallory Malificent",
+             Card("0000000000000000", 666, 4, 2025), 666_00)
+_add_account("Alice Allison", Card.generate_random(5, 2025), 1000_00)
+_add_account("Bobby McBobface", Card("0505050505050505", 123, 6, 2023), 10_00)
+_add_account("Billy Bazillionaire",
+             Card.generate_random(12, 2100), 1000000000000_00)
+
+
+def get_account(card: Card) -> Account:
+    """
+    Gets an account based on the provided card's card number, additionally verifying the other card information.
+
+    Parameters
+    ----
+       - `card` - The debit card of the account to be retrieved.
+
+    Returns
+    ----
+       The associated `Account` if it exists and the card information is valid.
+
+    Throws
+    ----
+       - `ValueError` if no account with the card number was found.
+       - `PermissionError` if the card information did not match the found account.
+    """
+    account = _db.get(card.number)
+    if account is None:
+        raise ValueError(
+            f"No matching account found for card number {card.number}.")
+    elif card != account.card:
+        raise PermissionError(
+            "Provided card details do not match the associated account.")
+    return card
--- server.py
+++ server.py
@@ -1,19 +1,34 @@
 import socketserver as ssv
+from typing import Optional
+from database import Account, get_account
+
 
 class Handler(ssv.StreamRequestHandler):
     """
     Parent reference: https://docs.python.org/3/library/socketserver.html#socketserver.StreamRequestHandler
     """
+
     def setup(self):
         super().setup()
-        # Then do setup stuff
-    
+        # If this is not None, handshake is done
+        self.session_key: Optional[bytes] = None
+        self.seq = 0  # Uses of session key
+        # If this is not None, account is authenticated
+        self.account: Optional[Account] = None
+
     def handle(self):
-        pass
+        if self.session_key is None:
+            # SLL handshake
+            return
+        elif self.account is None:
+            # User sign-in stuff
+            return
+        # Otherwise, accept commands
 
     def finish(self):
         super().finish()
         # Then do finishing stuff
+
 
 with ssv.ThreadingTCPServer(("localhost", 125), Handler) as server:
     server.serve_forever()
