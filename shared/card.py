dff74cc2628502693bc67680c0f8cf6a1b6c2bfb
2kai2kai2 <2kai2kai2@gmail.com>
2023-03-16 17:27:29 -0400

    Add PIN number to card data
    While not actually a part of the card, it is for purposes of our implementation due to the lack of a physical card to begin with.

--- shared/card.py
+++ shared/card.py
@@ -1,6 +1,7 @@
 """
 This file contains shared implementation for the communication and verification of cards.
 """
+import random
 
 
 def valid_card_num(number: str) -> bool:
@@ -19,24 +20,28 @@ class Card:
     `[0,54)` - Card Number (unsigned) \\
     `[54,64)` - CVC (unsigned) \\
     `[64,68)` - Month (unsigned) \\
-    `[68,78)` - Year (unsigned, starting with 0 as year 2000)
+    `[68,78)` - Year (unsigned, starting with 0 as year 2000) \\
+    `[78, 92)` - PIN (unsigned)
     """
-    BYTE_LEN = 10
+    BYTE_LEN = 12
     BIT_LEN = BYTE_LEN * 8
 
-    def __init__(self, number: str, cvc: int, month: int, year: int):
+    def __init__(self, number: str, cvc: int, month: int, year: int, pin: int):
         if not valid_card_num(number):
             raise ValueError("Invalid card number.")
         if cvc < 0 or 1000 <= cvc:
-            raise ValueError("Invalid CVC on card.")
+            raise OverflowError("Invalid CVC.")
         if month < 1 or 12 < month:
-            raise OverflowError("Invalid expiration month on card.")
+            raise OverflowError("Invalid expiration month.")
         if year < 2000 or 2000+2**10 <= year:
-            raise OverflowError("Invalid expiration year on card.")
+            raise OverflowError("Invalid expiration year.")
+        if pin < 0 or 10000 <= pin:
+            raise OverflowError("Invalid pin number provided.")
         self.number = number
         self.cvc = cvc
         self.month = month
         self.year = year
+        self.pin = pin
 
     def to_bytes(self) -> bytes:
         as_int: int = 0
@@ -45,12 +50,13 @@ class Card:
         as_int |= self.cvc << (Card.BIT_LEN-64)
         as_int |= self.month << (Card.BIT_LEN-68)
         as_int |= (self.year - 2000) << (Card.BIT_LEN-78)
+        as_int |= self.pin << (Card.BIT_LEN-92)
 
         return as_int.to_bytes(Card.BYTE_LEN, 'big')
 
     @classmethod
     def from_bytes(cls, data: bytes):
-        if len(data) != 10:
+        if len(data) != Card.BYTE_LEN:
             raise ValueError(
                 f"UserAuth->from_bytes must be bytes object of length {Card.BYTE_LEN} ({Card.BIT_LEN} bits)")
         as_int = int.from_bytes(data, 'big')
@@ -59,13 +65,27 @@ class Card:
         cvc = (as_int >> (Card.BIT_LEN-64)) & 0x3ff
         month = (as_int >> (Card.BIT_LEN-68)) & 0xf
         year = ((as_int >> (Card.BIT_LEN-78)) & 0x3ff) + 2000
+        pin = ((as_int >> (Card.BIT_LEN-92)) & 0x3fff)
 
-        return Card(card_num, cvc, month, year)
+        return Card(card_num, cvc, month, year, pin)
 
     def __eq__(self, other: object) -> bool:
         if not isinstance(other, Card):
             return False
-        return self.number == other.number and self.cvc == other.cvc and self.month == other.month and self.year == other.year
+        return self.number == other.number and self.cvc == other.cvc and self.month == other.month and self.year == other.year and self.pin == other.pin
 
     def __repr__(self) -> str:
-        return f"<Card: number:{self.number} CVC:{str(self.cvc).zfill(3)} expires:{str(self.month).zfill(2)}/{self.year}>"
+        return f"<Card: number:{self.number} CVC:{str(self.cvc).zfill(3)} expires:{str(self.month).zfill(2)}/{self.year} pin:{str(self.pin).zfill(4)}>"
+
+    @classmethod
+    def generate_random(cls, month: int, year: int):
+        if month < 1 or 12 < month:
+            raise OverflowError("Invalid expiration month on card.")
+        if year < 2000 or 2000+2**10 <= year:
+            raise OverflowError("Invalid expiration year on card.")
+        card_num = ""
+        while not valid_card_num(card_num):
+            card_num = str(random.randrange(0, 10**16))
+        cvc = random.randrange(0, 1000)
+        pin = random.randrange(0, 10000)
+        return Card(card_num, cvc, month, year, pin)
--- shared/card_test.py
+++ shared/card_test.py
@@ -1,15 +1,15 @@
-import card
+from card import Card
 
 if __name__ != "__main__":
     quit()
 
-a = card.Card("0000000000000000", 999, 12, 2999)
+a = Card("0000000000000000", 999, 12, 2999, 1234)
 print(a)
 a_bytes = a.to_bytes()
 print(a_bytes.hex())
-a_retrieved = card.Card.from_bytes(a_bytes)
+a_retrieved = Card.from_bytes(a_bytes)
 
-b = card.Card("0505050505050505", 999, 12, 2998)
+b = Card("0505050505050505", 999, 12, 2998, 1111)
 print(b)
 b_bytes = b.to_bytes()
 print(b_bytes.hex())
@@ -17,3 +17,8 @@ print(b_bytes.hex())
 assert a == a_retrieved
 assert b != a
 assert a_bytes != b_bytes
+
+for i in range(1000):
+    tmp = Card.generate_random(10, 2025)
+    tmp_bytes = tmp.to_bytes()
+    assert tmp == Card.from_bytes(tmp_bytes)
