42406091e7dc561cfdffa3c67e388e69bea9f038
Aidan McHugh <mchughaw@gmail.com>
2023-04-11 19:46:45 -0400

    ServerHello and EncryptedExtensions

--- shared/ssl_handshake.py
+++ shared/ssl_handshake.py
@@ -28,18 +28,27 @@ def marshal_list_of_ints(arr: list[int], elementLength: int = 1, maxLengthInByte
         ret += el.to_bytes(elementLength, "big")
     return ret
 
+def marshal_list_of_objects(arr: list[int], elementLength: int = 1, maxLengthInBytes: int = 2) -> bytes:
+    # Encode the length of objects
+    ret = (len(arr)).to_bytes(maxLengthInBytes, 'big')
+    # Encode every object sequenially
+    for el in arr:
+        ret += el.to_bytes(elementLength, "big")
+    return ret
+
 def unmarshal_list(msg: bytes, elementLength: int = 1, lengthFieldSize: int = 2, start: int = 0) -> Union[list,int]:
     count = int.from_bytes(msg[start:start+lengthFieldSize])
     ret = []
-    for i in range(count):
-        s = start+i*elementLength
+    for i in range(count//elementLength):
+        s = start+lengthFieldSize+i*elementLength
         ret.append(msg[s:s+elementLength])
-    return ret, lengthFieldSize + count * elementLength
+    return ret, lengthFieldSize + count
 
 class HandshakeType(IntEnum):
     # https://datatracker.ietf.org/doc/html/rfc8446#section-4
     client_hello = 1
     server_hello = 2
+    encrypted_extensions = 8
 
 class Handshake:
     def __init__(self, msg_type: int, msg_length: int):
@@ -74,6 +83,7 @@ class ExtensionType(IntEnum):
     signature_algorithms = 13
     supported_versions = 43
     key_share = 51
+    server_name = 0
 
 class SignatureAlgorithms(IntEnum):
     # A full list of signature algorithms is in RFC 8446, Section 4.2.3:
@@ -85,11 +95,20 @@ class SupportedGroups(IntEnum):
     ffdhe2048 = 0x0100
 
 class KeyShareEntry:
-    def __init__(self, group: int, key_exchange):
+    def __init__(self, group: int, key_exchange: bytes):
         # Make sure we were given a valid group
         assert(group in SupportedGroups._value2member_map_)
         self.group = group
         self.key_exchange = key_exchange
+
+    def toData(self) -> bytes:
+        return self.group.to_bytes(2, 'big') + len(self.key_exchange).to_bytes(2, 'big') + self.key_exchange
+
+    def fromData(self, data) -> int:
+        self.group = int.from_bytes(data[:2])
+        key_exchange_len = int.from_bytes(data[2:4])
+        self.key_exchange = data[4:4+key_exchange_len]
+        return 4 + key_exchange_len
 
 class KeyShare_ClientShares:
     def __init__(self, entries: list[KeyShareEntry]):
@@ -162,7 +181,7 @@ class ClientHello:
         # todo:: change this when more algorithms are implemented
         assert(DHKeyShare.group == SupportedGroups.ffdhe2048)
         # Set the client shares information
-        # self.extensions[ExtensionType.key_share] = KeyShare_ClientShares([DHKeyShare])
+        self.extensions.append(MakeExtension(ExtensionType.key_share, DHKeyShare.toData()))
 
     def marshal(self) -> bytes:
         # Here is the structure of this object:
@@ -189,6 +208,99 @@ class ClientHello:
         for ex in self.extensions:
             encoded_extensions += ex.marshal()
         # Add the extensions to the message
+        last = len(encoded_extensions).to_bytes(2, 'big')
+        last += encoded_extensions
+        # Finally we can push the handshake header info as 
+        # we know the length of the rest of the message
+        ret += last
+        self.handshake.msg_length = len(ret)
+        prefix = self.handshake.marshal()
+        ret = prefix + ret
+        return ret
+
+    def unmarshal(self, msg: bytes) -> bool:
+        # First we have the prefix that we can strip away
+        res = self.handshake.unmarshal(msg)
+        if not res: return False
+        if not self.handshake.msg_type == HandshakeType.client_hello: return False
+
+        try:
+            tracker = 4
+            # Next 2 bytes should be legacy version information 
+            self.legacy_version = int.from_bytes(msg[tracker:tracker+2])
+            tracker += 2
+            
+            # Next 32 have the random
+            self.random = msg[tracker:tracker+32]
+            tracker += 32
+
+            self.legacy_session_id, advance = unmarshal_list(msg, elementLength=1, lengthFieldSize=1, start=tracker)
+            tracker += advance
+            cs, advance = unmarshal_list(msg, elementLength=2, lengthFieldSize=2, start=tracker)
+            self.cipher_suites = [int.from_bytes(x) for x in cs]
+            tracker += advance
+            self.legacy_compression_methods, advance = unmarshal_list(msg, elementLength=1, lengthFieldSize=1, start=tracker)
+            tracker += advance
+
+            extensionFieldLength = int.from_bytes(msg[tracker:tracker+2])
+            tracker += 2
+            subtracker = 0
+            self.extensions = []
+            while subtracker < extensionFieldLength:
+                ex = Extension()
+                consumed = ex.unmarshal(msg, start=tracker+subtracker)
+                subtracker += consumed
+                self.extensions.append(ex)
+
+            return True
+        except Exception as ex:
+            print("[ERROR]", ex)
+            return False
+
+class ServerHello:
+    legacy_version = 0x0303
+    random = 0
+
+    cipher_suite = None
+
+    extensions = [
+        MakeExtension(ExtensionType.supported_versions, 0x0304.to_bytes(2, 'big')),
+    ]
+
+    handshake = Handshake(HandshakeType.server_hello, -1)
+
+    def populate(self, correspondingHello: ClientHello, selectedCipherSuite, randomFunc: Callable[[int], bytes] = urandom):
+        # Create 32 random bytes for the 'random' field (used as a Nonce)
+        self.random = randomFunc(32)
+        self.legacy_session_id = correspondingHello.legacy_session_id
+        if not selectedCipherSuite in correspondingHello.cipher_suites:
+            print("[WARN] Selected Cipher Suite {}, but the corresponding ClientHello message do not offer it")
+            return False
+        self.cipher_suite = selectedCipherSuite
+        return True
+
+    def marshal(self):
+        """
+        struct {
+            ProtocolVersion legacy_version = 0x0303;
+            Random random;
+            opaque legacy_session_id_echo<0..32>;
+            CipherSuite cipher_suite;
+            uint8 legacy_compression_method = 0;
+            Extension extensions<6..2^16-1>;
+        } ServerHello;
+        """
+        ret = self.legacy_version.to_bytes(2, "big")
+        ret += self.random
+        ret += marshal_list_of_bytes(self.legacy_session_id, elementLength=1, maxLengthInBytes=1)
+        ret += self.cipher_suite.to_bytes(2, "big")
+        # Legacy compression method
+        ret += (0).to_bytes(1, "big")
+        # Encode the extensions
+        encoded_extensions = bytes(0)
+        for ex in self.extensions:
+            encoded_extensions += ex.marshal()
+        # Add the extensions to the message
         ret += len(encoded_extensions).to_bytes(2, 'big')
         ret += encoded_extensions
         # Finally we can push the handshake header info as 
@@ -202,6 +314,7 @@ class ClientHello:
         # First we have the prefix that we can strip away
         res = self.handshake.unmarshal(msg)
         if not res: return False
+        if not self.handshake.msg_type == HandshakeType.server_hello: return False
 
         try:
             tracker = 4
@@ -213,38 +326,59 @@ class ClientHello:
             self.random = msg[tracker:tracker+32]
             tracker += 32
 
-            self.legacy_session_id, advance = unmarshal_list(msg, elementLength=1, maxLengthInBytes=1, start=tracker)
+            self.legacy_session_id, advance = unmarshal_list(msg, elementLength=1, lengthFieldSize=1, start=tracker)
             tracker += advance
-            self.cipher_suites, advance = unmarshal_list(msg, elementLength=2, maxLengthInBytes=2, start=tracker)
-            tracker += advance
-            self.legacy_compression_methods, advance = unmarshal_list(msg, elementLength=1, maxLengthInBytes=1, start=tracker)
-            tracker += advance
-
-            numExtensions = msg[tracker:tracker+2]
+            self.cipher_suite = int.from_bytes(msg[tracker:tracker+2])
             tracker += 2
+            legacy_compression_method = int.from_bytes(msg[tracker:tracker+1])
+            tracker += 1
+            if legacy_compression_method != 0: return False
+
+            extensionFieldLength = int.from_bytes(msg[tracker:tracker+2])
+            tracker += 2
+            subtracker = 0
             self.extensions = []
-            for i in range(numExtensions):
+            while subtracker < extensionFieldLength:
                 ex = Extension()
-                consumed = ex.unmarshal(msg, start=tracker)
-                tracker += consumed
+                consumed = ex.unmarshal(msg, start=tracker+subtracker)
+                subtracker += consumed
                 self.extensions.append(ex)
 
             return True
         except:
             return False
 
-class ServerHello:
-    legacy_version = 0x0303
-    def marshal(self):
-        """
-        struct {
-            ProtocolVersion legacy_version = 0x0303;
-            Random random;
-            opaque legacy_session_id_echo<0..32>;
-            CipherSuite cipher_suite;
-            uint8 legacy_compression_method = 0;
-            Extension extensions<6..2^16-1>;
-        } ServerHello;
-        """
-        ret = self.legacy_version.to_bytes(2, "big")
+class EncryptedExtensions:
+    handshake = Handshake(HandshakeType.encrypted_extensions, -1)
+    extensions = []
+    def populate(self, extensions: list[Extension]):
+        self.extensions = extensions
+    def marshal(self) -> bytes:
+        # Encode the extensions
+        encoded_extensions = bytes(0)
+        for ex in self.extensions:
+            encoded_extensions += ex.marshal()
+        ret = len(encoded_extensions).to_bytes(2, 'big') + encoded_extensions
+        self.handshake.msg_length = len(ret)
+        prefix = self.handshake.marshal()
+        return prefix + ret
+    def unmarshal(self, msg: bytes):
+        res = self.handshake.unmarshal(msg)
+        if not res: return False
+        if not self.handshake.msg_type == HandshakeType.encrypted_extensions: return False
+        try:
+            extensionFieldLength = int.from_bytes(msg[4:6])
+            subtracker = 0
+            self.extensions = []
+            while subtracker < extensionFieldLength:
+                ex = Extension()
+                consumed = ex.unmarshal(msg, start=6+subtracker)
+                subtracker += consumed
+                self.extensions.append(ex)
+            return True
+        except:
+            return False
+
+
+
 
--- shared/ssl_handshake_test.py
+++ shared/ssl_handshake_test.py
@@ -2,23 +2,66 @@
 from ssl_handshake import *
 
 def fixedRandom(bits):
-    b = bytes(31)
-    b += (0xaa).to_bytes()
+    b = bytes(17)
+    for i in range(0x1, 0xf + 1):
+        b += (0x11 * i).to_bytes()
     return b
 
 if __name__ == "__main__":
-    computedKey = KeyShareEntry(SupportedGroups.ffdhe2048, 57)
+    computedKey = KeyShareEntry(SupportedGroups.ffdhe2048, (57).to_bytes(1, 'big'))
     hello = ClientHello()
     hello.populate(computedKey, fixedRandom)
+    
+    # print(hello.marshal().hex())
 
-    example = 0x01000042030300000000000000000000000000000000000000000000000000000000000000aa0100000213a101000016002b00020304000d000400020201000a000400020100.to_bytes(70, 'big')
-    hello2 = ClientHello()
-    hello2.unmarshal(example)
+    # ------------------------------
     
     testIn = ClientHello()
     testOut = ClientHello()
 
     testIn.populate(computedKey, fixedRandom)
-    testOut.unmarshal(testIn.marshal())
-
+    data = testIn.marshal()
+    assert(testOut.unmarshal(data))
     assert(testOut.marshal() == testIn.marshal())
+
+    # ------------------------------
+
+    # Set up a ClientHello message for the ServerHello
+    example = 0x01000042030300000000000000000000000000000000000000000000000000000000000000aa0100000213a101000016002b00020304000d000400020201000a000400020100.to_bytes(70, 'big')
+    client = ClientHello()
+    client.unmarshal(example)
+
+    server = ServerHello()
+    server.populate(client, client.cipher_suites[0], fixedRandom)
+
+    server_example = 0x0200002f030300000000000000000000000000000000000000000000000000000000000000aa010013a1000006002b00020304.to_bytes(51, 'big')
+    server2 = ServerHello()
+    assert(server2.unmarshal(server_example))
+    assert(server2.unmarshal(server.marshal()))
+    assert(server2.marshal() == server.marshal())
+
+    # ------------------------------
+
+    ee = EncryptedExtensions()
+    ee2 = EncryptedExtensions()
+
+    example = [
+        MakeExtension(ExtensionType.server_name, bytes("Hello, world!", 'utf-8'))
+    ]
+
+    ee.populate(example)
+    # print(ee.marshal().hex())
+    ee2.unmarshal(ee.marshal())
+    assert(ee2.marshal() == ee.marshal())
+
+    example = [
+        MakeExtension(ExtensionType.server_name, bytes("Hello, world!", 'utf-8')),
+        MakeExtension(ExtensionType.supported_groups, marshal_list_of_ints([
+            SupportedGroups.ffdhe2048
+        ], 2, 2))
+    ]
+
+    ee.populate(example)
+    ee2.unmarshal(ee.marshal())
+    assert(ee2.marshal() == ee.marshal())
+
