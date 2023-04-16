69f0f1a220b233606b13f919cb4067f8b4593642
PREISB <preisb@rpi.edu>
2023-04-15 20:06:38 -0400

    Added Secrets To paillier.py

--- shared/paillier.py
+++ shared/paillier.py
@@ -1,10 +1,62 @@
-import random
+import math
+import secrets
+
+
+def make_keys() -> (int, int, int):
+    """
+        Calculates a valid p, q, and g value to be used for a Paillier Session.
+
+        Parameters
+        ----
+           None.
+
+        Returns
+        ----
+        p Private Key, prime.
+        q Private Key, prime.
+        g Generator is Set Z_n**2.
+        """
+    def is_prime(n):
+        for i in range(2, n):
+            if (n % i) == 0:
+                return False
+        return True
+
+    p = secrets.randbelow(1000)
+    while not is_prime(p) or p < 200:
+        p = secrets.randbelow(1000)
+
+    q = secrets.randbelow(1000)
+    while 1 != math.gcd(p * q, (p - 1) * (q - 1)) or p < 200 or not is_prime(q):
+        q = secrets.randbelow(1000)
+
+    n = p*q
+    g = secrets.randbelow(n*n)
+    while 1 != math.gcd(g, n*n):
+        g = secrets.randbelow(n*n)
+
+    return p, q, g
 
 
 # Paillier encryption using PyCrypto's PRNG
-def encrypt(m, n, g):
+def encrypt(m: int, n: int, g: int) -> int:
+    """
+        Encrypts Message 'm' with public keys 'n' and 'g'.
+
+        Parameters
+        ----
+           m Message PlainText
+           n p*q
+           g Public Key in set Z*_n^2
+
+        Returns
+        ----
+        c Message CphierText
+        """
     # Random number from PRNG
-    r = random.randrange(n)
+    r = secrets.randbelow(n)
+    while 1 != math.gcd(r, n):
+        r = secrets.randbelow(n)
     print("Random Number: " + str(r))
     # Begin Encryption
     print("Message: " + str(m))
@@ -13,58 +65,50 @@ def encrypt(m, n, g):
 
 
 # Decrypts the Paillier encryption using the given u = 53022
-def decrypt(c, p, q, g):
+def decrypt(c: int, p: int, q: int, g: int):
+    """
+        Decrypts Message 'c' with private keys 'p', 'q' and public key 'g'.
+
+        Parameters
+        ----
+        m Message CphierText
+        p Private Key
+        q Private Key
+        g Public Key in set Z*_n^2
+
+        Returns
+        ----
+        m Message Plaintext
+        """
     n = p * q
-
-    # Used for the Carmicheal Function
-    def lcm(x, y):
-        """This function takes two
-        integers and returns the L.C.M."""
-
-        # choose the greater number
-        greater = max(x, y)
-
-        while True:
-            if (greater % x == 0) and (greater % y == 0):
-                return greater
-                break
-            greater += 1
 
     # L function defined by Paillier PKC
     def L(um):
-        return (um - 1) / n
+        return (um - 1) // n
 
-    u = pow(L(pow(g, lcm(p - 1, q - 1)) % (n * n)), -1) % n
+    u = pow(L(pow(g, math.lcm(p - 1, q - 1), n * n)), -1, n)
     print("u:", u)
-    m = ((L(pow(c, lcm(p - 1, q - 1), n * n))) * u) % n
+    m = ((L(pow(c, math.lcm(p - 1, q - 1), n * n))) * u) % n
     return m
 
 
 # Sum of the messages by adding up the ciphertexts in a list or tuple
-def summation(ctxt):
+def summation(ctxt: tuple[int], n: int) -> int:
+    """
+        Does a summation of a list/tuple of Encrypted Messages.
+
+        Parameters
+        ----
+        ctext CphierText List
+        n: Public Key
+
+        Returns
+        ----
+        sum
+        """
     tally = 1
     for i in range(0, len(ctxt)):
         tally = tally * ctxt[i] % (n * n)
     return tally
 
 
-if __name__ == '__main__':
-
-    # Private key information (carmicheal function(n))
-    p = 293
-    q = 433
-    # Public key pair
-    n = p * q  # 126869
-    g = 6497955158
-
-    ctxts = []
-    # Begin Random Sums
-    for i in range(0, 1):
-        print("Iteration: m = " + str(i))
-        c = encrypt(random.choice((5, 5)), n, g)
-        print("Ciphertext: " + str(c))
-        print("")
-        ctxts.append(c)
-    # Tally Votes without decrypting them
-    print("The Cryptographic Tally of Votes: " + str(summation(ctxts)))
-    print("The Decryption of the Tally: " + str(decrypt(summation(ctxts), p, q, g) % n))
